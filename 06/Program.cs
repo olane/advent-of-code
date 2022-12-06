d6765010063d934eb3fbd33178a5cc47e3c8be2a
Oli Lane <olil4ne@gmail.com>
2022-12-06 13:24:09 +0000

    day 6

---
+++ 06/Program.cs
@@ -0,0 +1,25 @@
+﻿var input = File.ReadAllText("input.txt");
+
+Console.WriteLine(FirstUniqueSequence(input, 4));
+Console.WriteLine(FirstUniqueSequence(input, 14));
+
+static int FirstUniqueSequence(string input, int subsequenceLength)
+{
+    var index = 0;
+    var bufferIndex = 0;
+    var buffer = new char[subsequenceLength];
+    foreach (char c in input)
+    {
+        buffer[bufferIndex] = c;
+
+        bufferIndex = (bufferIndex + 1) % subsequenceLength;
+        index++;
+
+        if (index >= subsequenceLength && buffer.Distinct().Count() == subsequenceLength)
+        {
+            return index;
+        }
+    }
+
+    throw new Exception("Not found");
+}
