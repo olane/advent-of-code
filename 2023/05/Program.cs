cd4ee6ae7c8bdb49adf21a6a5ae9e35729b4bb0b
Oli Lane <olil4ne@gmail.com>
2023-12-05 09:02:09 +0000

    sort ranges and a few other bits

--- 2023/05/Program.cs
+++ 2023/05/Program.cs
@@ -1,13 +1,17 @@
 ﻿
 
+using System.Collections.Immutable;
+
 var lines = File.ReadLines("input.txt").ToArray();
 
-var seeds = lines[0].Substring(7).Split(" ").Select(long.Parse);
+var seeds = lines[0].Substring(7).Split(" ").Select(long.Parse).ToArray();
 
 var maps = GetMaps(lines.Skip(2));
 
-IEnumerable<Map> GetMaps(IEnumerable<string> lines)
+Dictionary<string, Map> GetMaps(IEnumerable<string> lines)
 {
+    var result = new Dictionary<string, Map>();
+
     string from = "";
     string to = "";
     var maps = new List<RangeMap>();
@@ -19,7 +23,7 @@ IEnumerable<Map> GetMaps(IEnumerable<string> lines)
             maps = [];
         }
         else if (string.IsNullOrWhiteSpace(line)){
-            yield return new Map(from, to, maps);
+            result[from] = new Map(from, to, maps.ToImmutableSortedSet(new RangeMapComp()));
         }
         else {
             var numbers = line.Split(" ").Select(long.Parse).ToArray();
@@ -27,15 +31,17 @@ IEnumerable<Map> GetMaps(IEnumerable<string> lines)
         }
     }
     
-    yield return new Map(from, to, maps);
+    result[from] = new Map(from, to, maps.ToImmutableSortedSet(new RangeMapComp()));
+
+    return result;
 }
 
-long FindLocation(IEnumerable<Map> maps, string currentProperty, long id) {
+long FindLocation(Dictionary<string, Map> maps, string currentProperty, long id) {
     if (currentProperty == "location") {
         return id;
     }
     else {
-        var map = maps.First(x => x.from == currentProperty);
+        var map = maps[currentProperty];
         var newId = map.DoMap(id);
         return FindLocation(maps, map.to, newId);
     }
@@ -54,25 +60,49 @@ IEnumerable<long> rangeEnumerator(long start, long extent) {
     }
 }
 
-var seedRanges = seeds.Chunk(2);
+var seedRanges = seeds.Chunk(2).Take(1);
 var seeds2 = seedRanges.SelectMany(x => rangeEnumerator(x.First(), x.Skip(1).First()));
 var seed2Locations = seeds2.Select(x => FindLocation(maps, "seed", x));
 Console.WriteLine(seed2Locations.Min());
 
 
 //records
-record Map(string from, string to, List<RangeMap> maps)
+record Map(string from, string to, ImmutableSortedSet<RangeMap> maps)
 {
     internal long DoMap(long id)
     {
-        var matchingRange = maps.SingleOrDefault(x => x.targetStart <= id && id < x.targetStart + x.length);
-        if (matchingRange != null) {
+        RangeMap? matchingRange = GetRange(id);
+        if (matchingRange != null)
+        {
             var offset = id - matchingRange.targetStart;
             return matchingRange.sourceStart + offset;
         }
         return id;
     }
+
+    private RangeMap? GetRange(long id)
+    {
+        foreach(var map in maps) {
+            if (map.targetStart <= id && id < map.targetStart + map.length) {
+                return map;
+            }
+            if (id >= map.targetStart + map.length) {
+                return null;
+            }
+        }
+        return null;
+    }
+}
+
+internal class RangeMapComp : IComparer<RangeMap>
+{
+    public int Compare(RangeMap x, RangeMap y)
+    {
+        return y.targetStart.CompareTo(x.targetStart);
+    }
 }
 
 
 record RangeMap(long sourceStart, long targetStart, long length);
+
+record Range(long start, long end);
