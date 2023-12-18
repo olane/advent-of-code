e756c726c482defbf9702963b1e2ff0f792acca3
Oli Lane <olil4ne@gmail.com>
2023-12-18 10:14:01 +0000

    lol works when you delete most of the code

--- 2023/18/Program.cs
+++ 2023/18/Program.cs
@@ -1,6 +1,5 @@
 ﻿
-
-var input = File.ReadAllLines("example.txt");
+var input = File.ReadAllLines("input.txt");
 
 var lines = input.Select(i =>
 {
@@ -30,170 +29,20 @@ Console.WriteLine(CountSize(lines2));
 long CountSize(Line[] lines)
 {
     var segments = WalkLines(lines).ToArray();
-    var minX = segments.Select(s => s.start.x).Min();
-    var minY = segments.Select(s => s.start.y).Min();
-    var maxX = segments.Select(s => s.start.x).Max();
-    var maxY = segments.Select(s => s.start.y).Max();
-
-    var verticalSegments = segments.Where(s => s.start.x == s.end.x).ToArray();
 
     long count = 0;
-    for (int y = minY; y <= maxY; y++)
+    for (int i = 0; i < segments.Length; i++)
     {
-        var relevantSegments = verticalSegments.Where(s => (s.start.y >= y && s.end.y <= y) || s.start.y <= y && s.end.y >= y).ToArray();
-
-        var ordered = relevantSegments.OrderBy(s => s.start.x).ToArray();
-
-        for (int i = 0; i < ordered.Length -1; i+=2)
-        {
-            var first = ordered[i];
-            var second = ordered[i+1];
-
-            if(first.start.y == second.start.y || first.start.y == second.end.y || first.end.y == second.start.y || first.end.y == second.end.y) {
-                // we are going along a horizontal, eat one "for free"
-                //count += second.start.x - first.start.x - 1;
-                i--;
-            }
-            else {
-                count += second.start.x - first.start.x - 1;
-            }
-        }
+        // shoelace formula
+        count += Determinant(segments[i].start, segments[i].end);
     }
 
-    return count + segments.Select(s => s.Length() - 1).Sum();
+    var sum = segments.Sum(s => s.Length());
+    return (count + sum) / 2 + 1;
 }
 
-int Count(char[,] arr, int width, int height, char c)
-{
-    var count = 0;
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            if (arr[x, y] == c)
-            {
-                count++;
-            }
-        }
-    }
-    return count;
-}
-
-void Flood(char[,] arr, (int x, int y) initPos, int width, int height, char c)
-{
-    var q = new Queue<(int x, int y)>();
-    q.Enqueue(initPos);
-
-    while (q.Count != 0)
-    {
-        var (x, y) = q.Dequeue();
-
-        if (y >= height) continue;
-        if (y < 0) continue;
-        if (x >= width) continue;
-        if (x < 0) continue;
-        if (arr[x, y] == c) continue;
-
-        arr[x, y] = c;
-        q.Enqueue((x - 1, y - 1));
-        q.Enqueue((x - 1, y));
-        q.Enqueue((x - 1, y + 1));
-        q.Enqueue((x, y + 1));
-        q.Enqueue((x, y - 1));
-        q.Enqueue((x + 1, y - 1));
-        q.Enqueue((x + 1, y));
-        q.Enqueue((x + 1, y + 1));
-    }
-}
-
-
-(char[,] arr, int width, int height, int xOffset, int yOffset) Paint(IEnumerable<Segment> lineSegments)
-{
-    var minX = lineSegments.Select(s => s.start.x).Min();
-    var minY = lineSegments.Select(s => s.start.y).Min();
-    var maxX = lineSegments.Select(s => s.start.x).Max();
-    var maxY = lineSegments.Select(s => s.start.y).Max();
-
-    var xTranslation = 0 - minX;
-    var yTranslation = 0 - minY;
-
-    var width = maxX + xTranslation + 1;
-    var height = maxY + yTranslation + 1;
-
-    var result = new char[width, height];
-    for (int i = 0; i < width; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
-            result[i, j] = '.';
-        }
-    }
-
-    foreach (var s in lineSegments)
-    {
-        if (s.start.x == s.end.x)
-        {
-            //vertical
-            for (int y = Math.Min(s.start.y, s.end.y); y <= Math.Max(s.start.y, s.end.y); y++)
-            {
-                result[s.start.x + xTranslation, y + yTranslation] = '#';
-            }
-        }
-        else
-        {
-            //horizontal
-            for (int x = Math.Min(s.start.x, s.end.x); x <= Math.Max(s.start.x, s.end.x); x++)
-            {
-                result[x + xTranslation, s.start.y + yTranslation] = '#';
-            }
-        }
-    }
-
-    return (result, width, height, xTranslation, yTranslation);
-}
-
-bool PointInSegment(Segment segment, Point point)
-{
-    if (segment.start.x == point.x && segment.end.x == point.x)
-    {
-        return point.y >= Math.Min(segment.start.y, segment.end.y) && point.y <= Math.Max(segment.start.y, segment.end.y);
-    }
-    else if (segment.start.y == point.y && segment.end.y == point.y)
-    {
-        return point.x >= Math.Min(segment.start.x, segment.end.x) && point.x <= Math.Max(segment.start.x, segment.end.x);
-    }
-    return false;
-}
-
-bool PointInShape(IEnumerable<Segment> segments, Point point)
-{
-    var y = point.y;
-    var verticalSegments = segments.Where(s => s.start.y != s.end.y);
-    var relevantSegments = verticalSegments.Where(s => (s.start.y >= y && s.end.y <= y) || s.start.y <= y && s.end.y >= y);
-
-    var maxX = relevantSegments.Select(s => s.start.x).Max();
-
-    //go right, counting winding until we're out
-    var x = point.x;
-    var winding = 0;
-    while (x <= maxX + 1)
-    {
-        var matchingUpSegment = relevantSegments.FirstOrDefault(s => s.start.x == x && s.start.y >= y && s.end.y <= y);
-        var matchingDownSegment = relevantSegments.FirstOrDefault(s => s.start.x == x && s.start.y <= y && s.end.y >= y);
-
-        if (matchingUpSegment != null && x != point.x)
-        {
-            winding++;
-        }
-        if (matchingDownSegment != null)
-        {
-            winding--;
-        }
-
-        x++;
-    }
-
-    return winding != 0;
+long Determinant(Point a, Point b) {
+    return a.x * b.y - b.x * a.y;
 }
 
 IEnumerable<Segment> WalkLines(IEnumerable<Line> lines)
@@ -208,21 +57,8 @@ IEnumerable<Segment> WalkLines(IEnumerable<Line> lines)
     }
 }
 
-static void Print(char[,] arr, int width, int height)
-{
-    for (int y = 0; y < height; y++)
-    {
-        var line = "";
-        for (int x = 0; x < width; x++)
-        {
-            line += arr[x, y];
-        }
-        Console.WriteLine(line.Substring(0, 200));
-    }
-}
-
 record Segment(Point start, Point end) {
-    public int Length() {
+    public long Length() {
         if(start.x == end.x) {
             return Math.Abs(start.y - end.y);
         }
@@ -230,9 +66,9 @@ record Segment(Point start, Point end) {
     }
 };
 
-record Point(int x, int y)
+record Point(long x, long y)
 {
-    public Point Add(int length, Direction d)
+    public Point Add(long length, Direction d)
     {
         return d switch
         {
