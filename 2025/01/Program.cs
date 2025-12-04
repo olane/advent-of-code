efa10e63b4e1956045927303317b463a75c0c350
Oli Lane <oli@gearset.com>
2025-12-04 12:32:04 +0000

    1.1

---
+++ 2025/01/Program.cs
@@ -0,0 +1,22 @@
+﻿
+var lines = File.ReadLines("./input.txt");
+var position = 50;
+var count = 0;
+
+foreach (var line in lines)
+{
+    position = Move(position, line);
+    if (position == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
+
+int Move(int current, string input)
+{
+    var sign = input[0] == 'L' ? -1 : 1;
+    var move = sign * int.Parse(input[1..]);
+    return (current + move) % 100;
+}
