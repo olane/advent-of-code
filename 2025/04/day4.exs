7a5419ba19594fc7625b273cc859e88d22681d01
Oli Lane <oli@gearset.com>
2025-12-09 10:13:30 +0000

    very basic extract of grid util (should use mix really)

--- 2025/04/day4.exs
+++ 2025/04/day4.exs
@@ -1,36 +1,4 @@
-defmodule Grid do
-  def from_nested_list(rows) do
-    for {row, y} <- Enum.with_index(rows),
-        {val, x} <- Enum.with_index(row),
-        into: %{} do
-      {{x, y}, val}
-    end
-  end
-
-  def from_string(string, mapping_fun \\ &Function.identity/1) do
-    string
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.map(fn line ->
-      line |> String.graphemes() |> Enum.map(mapping_fun)
-    end)
-    |> from_nested_list()
-  end
-
-  def neighbour_positions({x, y}) do
-    for dx <- -1..1, dy <- -1..1, {dx, dy} != {0, 0} do
-      {dx + x, dy + y}
-    end
-  end
-
-  def is_neighbour({x1, y1}, {x2, y2}) do
-    abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1 and (x1 != x2 or y1 != y2)
-  end
-
-  def neighbours(grid, pos) do
-    Map.filter(grid, fn {key, _} -> is_neighbour(key, pos) end)
-  end
-end
+Code.require_file("../util/grid.ex")
 
 defmodule Day4 do
   def part_1(grid) do
